678fee9a3af3bc4986dd6431340d5f4c8ee61d47
Damian Gałka <galka.damian.91@gmail.com>
2016-05-07 10:53:52 +0200

    change scripts

---
+++ rush-hour-webapp/src/main/resources_os/scripts/buildApp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+APP=./rush-hour-webapp/
+MVN_CMD=$M2_HOME/bin/mvn
+
+$MVN_CMD clean package
+
+rm -rf startApp
+mkdir startApp
+cp $APP/target/rush-hour-webapp-1.0-SNAPSHOT.jar startApp/
+cp -r $APP/src/main/resources_os startApp/config
+mv startApp/config/scripts/* startApp/
