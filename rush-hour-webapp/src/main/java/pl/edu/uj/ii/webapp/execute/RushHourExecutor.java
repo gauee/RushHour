5f01538b091374217d588207efcd02a662339273
Damian Gałka <galka.damian.91@gmail.com>
2016-06-03 01:35:15 +0200

    sorts testcases by id

--- rush-hour-webapp/src/main/java/pl/edu/uj/ii/webapp/execute/RushHourExecutor.java
+++ rush-hour-webapp/src/main/java/pl/edu/uj/ii/webapp/execute/RushHourExecutor.java
@@ -22,14 +22,15 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
 import static org.apache.commons.io.FilenameUtils.removeExtension;
 import static pl.edu.uj.ii.webapp.AppConfig.CONFIG;
 
@@ -50,6 +51,7 @@ public class RushHourExecutor {
         List<TestCaseDetails> testCaseDetailses = Lists.newLinkedList();
         try {
             List<TestCase> testCases = loadTestCases();
+            Collections.sort(testCases, (o1, o2) -> o1.getId().compareTo(o2.getId()));
             ExecutionTask executionTask = this.taskFactory.createTask(solutionTask);
             if (executionTask == null) {
                 LOGGER.warn("Cannot compile uploaded file.");
@@ -96,7 +98,7 @@ public class RushHourExecutor {
             return Files.list(Paths.get(getClass().getClassLoader().getResource(CONFIG.getTestCasesDir()).toURI()))
                     .map(this::convertToTestCase)
                     .filter(Objects::nonNull)
-                    .collect(Collectors.toList());
+                    .collect(toList());
         } catch (IOException | URISyntaxException e) {
             LOGGER.warn("Cannot load test cases from " + CONFIG.getTestCasesDir());
         }
