4fd6829cdf4e60b0e59df762cdd99532b619a1d8
Damian Gałka <galka.damian.91@gmail.com>
2016-06-07 21:33:33 +0200

    add limit on execution message

--- rush-hour-webapp/src/main/java/pl/edu/uj/ii/webapp/solution/Scheduler.java
+++ rush-hour-webapp/src/main/java/pl/edu/uj/ii/webapp/solution/Scheduler.java
@@ -98,7 +98,7 @@ public class Scheduler extends Thread {
                 testCaseDetails.getResultDetail()
                         .withDuration(testResult.getDuration())
                         .withMoves(testResult.getStepsOfAllTestCases())
-                        .withMsg(testResult.getExecutionMessage().isEmpty() ? EXECUTED : testResult.getExecutionMessage().substring(0, 128));
+                        .withMsg(testResult.getExecutionMessage().isEmpty() ? EXECUTED : testResult.getExecutionMessage().substring(0, Math.min(128, testResult.getExecutionMessage().length())));
             } catch (InterruptedException | ExecutionException e) {
                 LOGGER.error("Exception occurred during execution solution " + solutionId, e);
                 testCaseDetails.getResultDetail()
